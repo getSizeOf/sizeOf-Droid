6361a020e0a07e902987e52536acb57ee2c44eaf
Adam Walsh <adamwalsh14@gmail.com>
2015-04-18 18:44:46 -0400

    Now full screen

--- project/app/src/main/java/com/sizeof/sizeof/CaptureActivity.java
+++ project/app/src/main/java/com/sizeof/sizeof/CaptureActivity.java
@@ -4,16 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class CaptureActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_capture);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_capture);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
