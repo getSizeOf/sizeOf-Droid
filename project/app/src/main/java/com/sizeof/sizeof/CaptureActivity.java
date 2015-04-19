1ce6954ac06c892d11c56770f4aae8d39d08f259
Adam Walsh <adamwalsh14@gmail.com>
2015-04-18 20:02:25 -0400

    Added logic to connect camera to surfaceView and handle 5001 error using callback overrides

--- project/app/src/main/java/com/sizeof/sizeof/CaptureActivity.java
+++ project/app/src/main/java/com/sizeof/sizeof/CaptureActivity.java
@@ -1,14 +1,23 @@
 package com.sizeof.sizeof;
 
-import android.support.v7.app.ActionBarActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.io.IOException;
 
-public class CaptureActivity extends ActionBarActivity {
+
+public class CaptureActivity extends ActionBarActivity implements SurfaceHolder.Callback {
+    private Camera cam;
+    private SurfaceHolder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,9 @@ public class CaptureActivity extends ActionBarActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //set content view AFTER ABOVE sequence (to avoid crash)
         this.setContentView(R.layout.activity_capture);
+        SurfaceView view = (SurfaceView)(findViewById(R.id.surfaceView));
+        this.holder = view.getHolder();//FIXME null check
+        this.holder.addCallback(this);
     }
 
     @Override
@@ -41,5 +53,62 @@ public class CaptureActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.cam = Camera.open();
+        if (this.cam == null)
+            return;//TODO
+        try {
+            this.cam.setPreviewDisplay(this.holder);
+            this.cam.startPreview();
+            System.out.println("started");
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.cam.stopPreview();
+        if (this.cam != null)
+            this.cam.release();
+    }
+
+    @Override
+    public void surfaceCreated(final SurfaceHolder surfaceHolder) {
+        try {
+            this.cam.setPreviewDisplay(surfaceHolder);
+        }
+        catch (Exception e) {
+            // ...
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(final SurfaceHolder surfaceHolder) {
+        // ...
+    }
+
+    @Override
+    public void surfaceChanged(final SurfaceHolder surfaceHolder, final int format, final int width, final int height) {
+        if (this.holder.getSurface() == null) {
+            // ...
+            return ;
+        }
+
+        try {
+            this.cam.stopPreview();
+            // ...
+            this.cam.setPreviewDisplay(this.holder);
+            this.cam.startPreview();
+        }
+        catch (Exception e) {
+            // ...
+        }
     }
 }
